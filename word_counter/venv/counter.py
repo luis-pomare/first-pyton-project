6047f1f07d09b46113a9fc4590e6b7c04502846c
Luis Pomare <luispomare92@gmail.com>
2025-11-19 07:30:24 -0500

    Create counter.py code plan with comments

---
+++ word_counter/venv/counter.py
@@ -0,0 +1,13 @@
+ # Program to count words in a text file
+ 
+
+# 1. Ask the user for the path to a text file.
+
+# 2. Read the contents of the file.
+
+# 3. Separate into words.
+
+# Count total number of words.
+
+
+# (Optional) Display the 10 most frequent words and their count. 
